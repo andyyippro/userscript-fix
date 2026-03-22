dee4520707b64138d57a9651a83748d1c6359021
leishen <zqwvyk@gmail.com>
2026-03-22 16:43:24 +0800

    Add HUNTC to FANZA no-prefix list and bump version

--- JAV 添加跳转.user.js
+++ JAV 添加跳转.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAV 添加跳转
 // @namespace    https://github.com/andyyippro/userscript-fix
-// @version      1.2.20
+// @version      1.2.21
 // @author       andyyippro
 // @description  为 JavDB、JavBus、JavLibrary 这三个站点添加跳转在线观看的链接
 // @license      MIT
@@ -123,7 +123,8 @@
     "dvmm",
     "midv",
     "mdvr",
-    "ngod"
+    "ngod",
+    "huntc"
   ]);
   const FANZA_SPECIAL_PREFIXES = /* @__PURE__ */ new Map([
     // 在这里填写需要改写前缀的 FANZA 番号，格式为 [原前缀, 目标前缀]，例如 ["sspo", "h_261"]
