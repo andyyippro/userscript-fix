ddb35971c59872e7201cade111537eae85efc672
leishen <zqwvyk@gmail.com>
2026-03-25 21:29:34 +0800

    Add SNOS to FANZA no-prefix exceptions

--- JAV 添加跳转.user.js
+++ JAV 添加跳转.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAV 添加跳转
 // @namespace    https://github.com/andyyippro/userscript-fix
-// @version      1.3.12
+// @version      1.3.13
 // @author       andyyippro
 // @description  为 JavDB、JavBus、JavLibrary 这三个站点添加跳转在线观看的链接
 // @license      MIT
@@ -134,7 +134,8 @@
     "flav",
     "blk",
     "kws",
-    "avop"
+    "avop",
+    "snos"
   ]);
   const FANZA_SPECIAL_PREFIXES = /* @__PURE__ */ new Map([
     // 在这里填写需要改写前缀的 FANZA 番号，格式为 [原前缀, 目标前缀]，例如 ["sspo", "h_261"]
