fe158b73998190d7b641346bf51aa55d79e83cca
leishen <zqwvyk@gmail.com>
2026-03-25 22:04:19 +0800

    Add CAWD to FANZA no-prefix list

--- JAV 添加跳转.user.js
+++ JAV 添加跳转.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAV 添加跳转
 // @namespace    https://github.com/andyyippro/userscript-fix
-// @version      1.3.15
+// @version      1.3.16
 // @author       andyyippro
 // @description  为 JavDB、JavBus、JavLibrary 这三个站点添加跳转在线观看的链接
 // @license      MIT
@@ -137,7 +137,8 @@
     "avop",
     "snos",
     "pred",
-    "ndra"
+    "ndra",
+    "cawd"
   ]);
   const FANZA_SPECIAL_PREFIXES = /* @__PURE__ */ new Map([
     // 在这里填写需要改写前缀的 FANZA 番号，格式为 [原前缀, 目标前缀]，例如 ["sspo", "h_261"]
