1540328af947f9b54338996aa0b4aca35da8031a
leishen <zqwvyk@gmail.com>
2026-03-25 22:45:21 +0800

    Add SSNI to FANZA no-prefix list and bump version

--- JAV 添加跳转.user.js
+++ JAV 添加跳转.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAV 添加跳转
 // @namespace    https://github.com/andyyippro/userscript-fix
-// @version      1.3.17
+// @version      1.3.18
 // @author       andyyippro
 // @description  为 JavDB、JavBus、JavLibrary 这三个站点添加跳转在线观看的链接
 // @license      MIT
@@ -139,7 +139,8 @@
     "pred",
     "ndra",
     "cawd",
-    "sone"
+    "sone",
+    "ssni"
   ]);
   const FANZA_SPECIAL_PREFIXES = /* @__PURE__ */ new Map([
     // 在这里填写需要改写前缀的 FANZA 番号，格式为 [原前缀, 目标前缀]，例如 ["sspo", "h_261"]
